7279e63d13aa5dc357eb4fbd6e9d3566182f5bad
Anas Zughayyar <anas.ezzughayyar@gmail.com>
2024-12-09 15:34:57 +0200

    new

---
+++ src/main/java/com/assignment/routes/DaikichiController.java
@@ -0,0 +1,37 @@
+package com.assignment.routes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@SpringBootApplication
+@RestController
+public class DaikichiController {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaikichiController.class, args);
+	}
+
+	@RequestMapping(value="/daikichi", method=RequestMethod.GET)
+	public String daikichi() {
+		return "Welcome!";
+	}
+
+	@RequestMapping(value = "/daikichi/today", method=RequestMethod.GET)
+	public String today() {
+		return "Today you will find luck in all your endeavors!";
+	}
+
+	@RequestMapping(value = "/daikichi/tommorow", method=RequestMethod.GET)
+	public String tommorow() {
+		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+	
+	
+
+	
+
+}
---
+++ src/test/java/com/assignment/routes/RoutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.routes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
